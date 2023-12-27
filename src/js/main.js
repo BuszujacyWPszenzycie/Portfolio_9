1c9ef2235f2645b6bfaf54e6f80925977baa36cb
Marcin <marcin.szturminski@gmail.com>
2023-12-27 15:34:25 +0100

    finished portfolio section

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+// Function for picture in About section
+
 window.addEventListener('scroll', function () {
 	let scrollValue = window.scrollY
 	const aboutImg = document.querySelector('.about__img')
@@ -7,4 +9,21 @@ window.addEventListener('scroll', function () {
 	} else {
 		aboutImg.style.transform = 'scale(' + scaleValue + ')'
 	}
+})
+
+// Function for items in Portfolio
+
+window.addEventListener('scroll', function () {
+	const allPortfolioItems = document.querySelectorAll('.portfolio__item')
+	let scrollValue = window.scrollY
+	let scaleValue = 0.5 + scrollValue / 5000
+	let opacityValue = 0.55 + scrollValue / 7000
+	allPortfolioItems.forEach(item => {
+		if (parseFloat(scaleValue) > 1.1) {
+			item.style.transform = 'scale(1.1)'
+		} else {
+			item.style.transform = 'scale(' + scaleValue + ')'
+		}
+		item.style.opacity = opacityValue
+	})
 })
